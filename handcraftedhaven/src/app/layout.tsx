5a2324999921ac1d13565f71d115b140eba83d2c
joy935 <tupaiv@gmail.com>
2025-03-19 21:13:08 +0100

    initialized project

---
+++ handcraftedhaven/src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Inter, Poppins, Fira_Code } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["400", "600", "700"],
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const firaCode = Fira_Code({
+  variable: "--font-fira-code",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Handcrafted Haven",
+  description: "Marketplace for handmade goods"
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} ${poppins.variable} ${firaCode.variable}`}>
+        <main className="min-h-screen bg-background text-fg-light">
+        </main>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ handcraftedhaven/src/app/page.tsx
@@ -0,0 +1,8 @@
+import './globals.css';
+import { Inter } from 'next/font/google';
+
+export default function Home() {
+  return (
+    <h1>Title</h1>
+  );
+}
