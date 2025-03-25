21c4596edd1a588d5648ab21b88e2a4579e7d21f
joy935 <tupaiv@gmail.com>
2025-03-25 11:53:53 +0100

    built login and registration forms

--- handcraftedhaven/src/app/register/page.tsx
+++ handcraftedhaven/src/app/register/page.tsx
@@ -1,5 +1,9 @@
+import RegisterForm from "@/components/ui/register-form";
+
 export default function Register() {
     return (
-        <h1>Register Page</h1>
+        <main className="flex items-center justify-center md:h-screen py-6">  
+            <RegisterForm />
+        </main>
     );
 }
---
+++ handcraftedhaven/src/components/ui/login-form.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { Button } from "./Button";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+// add authentication logic here
+
+export default function LoginForm() {
+    const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get('callbackUrl') || '/';
+
+    return (
+        <form className="w-full max-w-sm space-y-5 p-6 bg-white rounded-xl shadow-md">
+            <h1 className="text-2xl font-semibold text-center">Log In</h1>
+            <div className="flex-1 space-y-4">
+                <div className="border border-gray-200 rounded-lg px-4 py-3">
+                    <input className="w-full bg-transparent outline-none text-sm" id="email" type="email" name="email" placeholder="Email address" required />
+                </div>
+                <div className="border border-gray-200 rounded-lg px-4 py-3">
+                    <input className="w-full bg-transparent outline-none text-sm" id="password" type="password" name="password" placeholder="Password" minLength={6} required />
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <input type="hidden" name="redirectTo" value={callbackUrl} />
+                <Button variant="login">Log In</Button>
+            </div>
+
+            <hr className="border-t border-gray-200" />
+
+            <div className="text-center">
+                    <p className="text-sm mb-3">Not a member yet?</p>
+                <Link href="/register">
+                    <Button variant="register">Register</Button></Link>
+            </div>
+        </form>
+    );
+}
