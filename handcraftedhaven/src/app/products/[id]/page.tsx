e502010fd48992d7b687642906e024ac71194322
andreahanzel <trky.andrea@gmail.com>
2025-04-01 14:13:22 -0700

    fixed separate generateMetadata to fix client/server component conflict

--- handcraftedhaven/src/app/products/[id]/page.tsx
+++ handcraftedhaven/src/app/products/[id]/page.tsx
@@ -11,7 +11,14 @@ import { useCart } from "@/contexts/CartContext";
 import { CheckCircle, ShoppingBag } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 
-export default function ProductDetailPage({ params }: { params: { id: string } }) {
+type ProductDetailPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default function ProductDetailPage({ params }: ProductDetailPageProps) {
+
   const id = parseInt(params.id);
   const { addToCart } = useCart();
   
@@ -156,14 +163,14 @@ export default function ProductDetailPage({ params }: { params: { id: string } }
                     aria-label="Add to wishlist"
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                         viewBox="0 0 24 24" strokeWidth={1.5}
-                         stroke="currentColor" className="w-6 h-6 text-neonPink">
-                      <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/>
-                    </svg>
-                  </button>
-                </div>
-                
+                        viewBox="0 0 24 24" strokeWidth={1.5}
+                        stroke="currentColor" className="w-6 h-6 text-neonPink">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/>
+                  </svg>
+                </button>
+              </div>
+              
                 {/* Added to cart notification */}
                 <AnimatePresence>
                   {addedToCart && (
